5aac9268e7977552bf8f15299b8b461930c6af45
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2020-11-13 09:33:47 -0500

    Update run.sh

--- Pi-Top/run.sh
+++ Pi-Top/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/with-contenv bashio
 CONFIG_PATH=/data/options.json
+$token = ""
 $token = $(jq --raw-output ".API_Token" $CONFIG_PATH)
 $url = $(jq --raw-output ".URL" $CONFIG_PATH)
 $running = $true
