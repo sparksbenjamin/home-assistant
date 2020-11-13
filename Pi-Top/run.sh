ad4b71648f1cd17b01006e0c94bfc36c1dc19ce9
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2020-11-13 09:15:22 -0500

    Pi-Hole Update

--- Pi-Top/run.sh
+++ Pi-Top/run.sh
@@ -2,5 +2,12 @@
 CONFIG_PATH=/data/options.json
 $token = $(jq --raw-output ".API_Token" $CONFIG_PATH)
 $url = $(jq --raw-output ".URL" $CONFIG_PATH)
-echo $token
+$running = $true
 
+while $running;do
+	echo $token
+	echo Hello world!
+
+
+
+done
