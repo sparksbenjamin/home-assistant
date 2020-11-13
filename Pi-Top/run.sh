6904ea44a9ef3820c95969d79c4643f4bcd87a84
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2020-11-13 01:52:29 -0500

    Update for API

--- Pi-Top/run.sh
+++ Pi-Top/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/with-contenv bashio
 CONFIG_PATH=/data/options.json
-echo $(jq --raw-output ".API Token" $CONFIG_PATH)
+echo $(jq --raw-output ".API_Token" $CONFIG_PATH)
 echo Hello world!
